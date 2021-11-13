b6d31999503bb8ec870b01e14089af89cea6303b
Alex Goldberg <vlexvnder@gmail.com>
2021-11-13 14:13:27 -0800

    added safeSearch API route and functionality

--- API/api.py
+++ API/api.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restx import Resource, Api
+from gcp_client import *
 
 app = Flask(__name__)
 api = Api(app)
 
-@api.route('/categorizeImage')
-class CategorizeImage(Resource):
-    def post(self, image_uri):
-        return image_uri
+@api.route('/safeSearch')
+class safeSearch(Resource):
+    def post(self):
+        """Uses the Google Cloud Vision API to detect safe search properties in the image.
+
+        Returns:
+            JSON: A JSON object containing the safe search properties of the image.
+        """
+        image_uri = request.form['image_uri']
+        return jsonify(detect_safe_search_uri(image_uri))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='127.0.0.1')
---
+++ API/gcp_client.py
@@ -0,0 +1,31 @@
+from google.cloud import vision
+
+def detect_safe_search_uri(uri):
+    """Detects unsafe features in the file located in Google Cloud Storage or
+    on the Web.
+    
+    From: https://cloud.google.com/vision/docs/detecting-safe-search
+    """
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = uri
+
+    response = client.safe_search_detection(image=image)
+    safe = response.safe_search_annotation
+
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                       'LIKELY', 'VERY_LIKELY')
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    return {
+        'adult': likelihood_name[safe.adult],
+        'medical': likelihood_name[safe.medical],
+        'spoofed': likelihood_name[safe.spoof],
+        'violence': likelihood_name[safe.violence],
+        'racy': likelihood_name[safe.racy]
+    }
+    
