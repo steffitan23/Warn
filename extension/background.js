39db7a80597982b8501f67eaca896948e6d6c24b
steffitan23 <tan.liwen.steffi@dhs.sg>
2021-11-14 04:34:24 -0800

    remove offending element

--- extension/background.js
+++ extension/background.js
@@ -32,15 +32,12 @@ function findTriggers() {
     }).then((response) => response.json());
   }
   async function parseHtmlForImgs(element) {
-    function checkForTriggers(response) {
+    function checkForTriggers(response, deleteElement) {
       response.forEach((trigger) => {
         console.log(trigger);
         chrome.storage.sync.get(null, (stored) => {
           if (trigger.name == "Violence" && stored["violence"]) {
-            element.documentElement.innerHTML = "";
-            while (element.firstChild) {
-              element.removeChild(document.firstChild);
-            }
+            deleteElement.remove();
             console.log("VIOLENCE FOUND");
           }
         });
@@ -56,7 +53,7 @@ function findTriggers() {
       ) {
         console.log(urlValue);
         var d = await post_request_image(APP_IP + "/awsModeration", urlValue);
-        checkForTriggers(d);
+        checkForTriggers(d, imgSrcUrls[i]);
       }
     }
   }
