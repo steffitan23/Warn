4c711e8ebbf1db48dece5eb465a62ff4b8e2c3e1
Alex Goldberg <vlexvnder@gmail.com>
2021-11-14 00:31:10 -0800

    updated requests.js to use fetch

--- API/api.py
+++ API/api.py
@@ -4,10 +4,11 @@ from gcp_client import *
 from aws_client import api as aws_moderation_label
 from text_analysis import hate_check, unwanted_words_check
 
-#hate_check("test") #warm up the API
+# hate_check("test") #warm up the API
 
 app = Flask(__name__)
 api = Api(app)
+
 
 @api.route('/hateSpeech')
 @api.doc(params={'text': 'A string of text'})
@@ -15,12 +16,13 @@ class HateSpeech(Resource):
     def post(self):
         """
         Detects whether a string of text is hate speech.
-        
+
         Returns:
             JSON: Returns the hate speech score of the text
         """
         text = request.form['text']
         return jsonify(hate_check(text))
+
 
 @api.route('/unwantedWords')
 @api.doc(params={'text': 'A string of text', 'unwanted_words': 'A list of unwanted words'})
@@ -29,6 +31,7 @@ class UnwantedWords(Resource):
         text = request.form['text']
         unwanted_words = request.form['unwanted_words']
         return jsonify(unwanted_words_check(text, unwanted_words))
+
 
 @api.route('/gcpSafeSearch')
 @api.doc(params={'image_uri': 'A link to an image'})
@@ -42,6 +45,7 @@ class SafeSearch(Resource):
         image_uri = request.form['image_uri']
         return jsonify(detect_safe_search_uri(image_uri))
 
+
 @api.route('/awsModeration')
 @api.doc(params={'image_uri': 'A link to an image'})
 class AWSModeration(Resource):
@@ -54,7 +58,6 @@ class AWSModeration(Resource):
         """
         image_uri = request.form['image_uri']
         return jsonify(aws_moderation_label(image_uri))
-
 
 
 if __name__ == "__main__":
