9593f10c553ae93272ca1caaca4a8fefcfd1c0ff
steffitan23 <tan.liwen.steffi@dhs.sg>
2021-11-14 02:40:38 -0800

    fixed post request & execute function on page load

--- extension/popup.js
+++ extension/popup.js
@@ -1,14 +1,3 @@
-let getTriggers = document.getElementById("getTriggers"); //guse dom to select button
-
-getTriggers.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: findTriggers,
-  });
-});
-
 const checkboxes = document.querySelectorAll(
   "input[type=checkbox][name=trigger-name]"
 );
@@ -21,17 +10,3 @@ checkboxes.forEach((checkbox) => {
     checkbox.checked = e[checkbox.value];
   });
 });
-
-function findTriggers() {
-  function parseHtmlString(element) {
-    var imgSrcUrls = element.getElementsByTagName("img");
-    for (var i = 0; i < imgSrcUrls.length; i++) {
-      var urlValue = imgSrcUrls[i].getAttribute("src");
-      if (urlValue && imgSrcUrls[i].clientHeight > 50 && imgSrcUrls[i].clientWidth > 50) {
-        console.log(urlValue);
-      }
-    }
-  }
-
-  parseHtmlString(document);
-}
